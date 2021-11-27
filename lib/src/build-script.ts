ea943d054d6df248eebe696605cadbd4b869f9b3
Aaron Klinker <aaronklinker1@gmail.com>
2021-11-27 09:26:32 -0600

    fix: wait for child script builds before reloading the extension

--- lib/src/build-script.ts
+++ lib/src/build-script.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import * as Vite from "vite";
+import { HookWaiter } from "./hook-waiter";
 
 export interface BuildScriptConfig {
   inputAbsPath: string;
@@ -8,22 +9,28 @@ export interface BuildScriptConfig {
   watch: boolean;
 }
 
-export async function buildScript(config: BuildScriptConfig) {
+export async function buildScript(
+  config: BuildScriptConfig,
+  hookWaiter: HookWaiter
+) {
   const filename = path.basename(config.outputRelPath);
   const outDir = path.resolve(
     config.vite.build?.outDir ?? process.cwd(),
     path.join(config.outputRelPath, "..")
   );
+  const plugins =
+    config.vite.plugins?.filter(
+      (plugin) =>
+        plugin &&
+        (!("name" in plugin) || plugin.name !== "vite-plugin-web-extension")
+    ) ?? [];
+  plugins.push(hookWaiter.plugin());
   await Vite.build({
     root: config.vite.root,
     clearScreen: false,
     mode: config.vite.mode,
     resolve: config.vite.resolve,
-    plugins: config.vite.plugins?.filter(
-      (plugin) =>
-        plugin &&
-        (!("name" in plugin) || plugin.name !== "vite-plugin-web-extension")
-    ),
+    plugins,
     define: config.vite.define,
     build: {
       ...config.vite.build,
