f4a7c2bcedb9aec6db56dd49b8e607e0f5d74273
Aaron Klinker <aaronklinker1@gmail.com>
2021-12-01 10:37:04 -0600

    fix: remove debug log

--- lib/index.ts
+++ lib/index.ts
@@ -146,7 +146,6 @@ export default function browserExtension<T>(
           )
         ),
       });
-      console.log(generatedScriptInputs[generatedScriptInputs.length - 1]);
     };
 
     const transformScripts = (object: any, key: string) => {
