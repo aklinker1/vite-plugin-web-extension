0be4fc682ae702693e01a88056e4d6053763719c
Aaron <aklinker1@users.noreply.github.com>
2022-09-02 13:30:45 -0500

    chore: convert E2E tests to use `vitest` instead of `jest` (#38)

--- packages/e2e/utils/expect-manifest.ts
+++ packages/e2e/utils/expect-manifest.ts
@@ -1,4 +1,5 @@
 import { readJsonFile } from "vite-plugin-web-extension";
+import { expect } from "vitest";
 
 export function expectManifest(expectedContent: any) {
   const actualContent = readJsonFile("dist/manifest.json");
