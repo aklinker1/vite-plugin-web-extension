7fdc5a52a28cdc8a8bf999da378dcf9b411b455f
Aaron Klinker <aklinker1@users.noreply.github.com>
2022-09-09 19:39:19 -0500

    cleanup

--- packages/vite-plugin-web-extension/src/utils/paths.ts
+++ packages/vite-plugin-web-extension/src/utils/paths.ts
@@ -1,10 +1,12 @@
 import { InlineConfig } from "vite";
 import path from "node:path";
 
+// TODO: Test
 export function getRootDir(config: InlineConfig): string {
   return path.resolve(process.cwd(), config.root ?? process.cwd());
 }
 
+// TODO: Test
 export function getOutDir(config: InlineConfig): string {
   const outDir = config.build?.outDir ?? "dist";
   return path.resolve(getRootDir(config), outDir);
