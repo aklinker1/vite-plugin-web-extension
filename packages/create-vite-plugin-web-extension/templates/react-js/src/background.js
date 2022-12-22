f75bc1c747486e5b7a129c58aa19e21dced02daf
Aaron Klinker <aklinker1@users.noreply.github.com>
2022-12-21 19:52:01 -0600

    Delete failed react template

--- packages/create-vite-plugin-web-extension/templates/react-js/src/background.js
+++
@@ -1,7 +0,0 @@
-import browser from "webextension-polyfill";
-
-console.log("Hello from the background!");
-
-browser.runtime.onInstalled.addListener((details) => {
-  console.log("Extension installed:", details);
-});
