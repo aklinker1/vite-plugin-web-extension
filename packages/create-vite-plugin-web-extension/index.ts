73a12de5dae9da8a1d9228636c69ab11e6eb46f0
Aaron Klinker <aklinker1@users.noreply.github.com>
2022-12-21 20:08:04 -0600

    connect commander cli to `initProject`

--- packages/create-vite-plugin-web-extension/index.ts
+++ packages/create-vite-plugin-web-extension/index.ts
@@ -1,17 +1,24 @@
 import { program } from "commander";
 import { initProject } from "./src/initProject";
 
-// program
-//   .option("-s, --separator <char>");
+program
+  .argument("[project-name]")
+  .option("-t, --template <template>")
+  .option("-p, --package-manager <package-manager>", "npm, pnpm, or yarn")
+  .option(
+    "--branch <branch>",
+    "The branch to look for templates on. This is only useful during development."
+  )
+  .parse();
 
-program.parse();
+const options = program.opts();
+console.log(options);
 
 initProject({
-  projectName: "test",
-  forceOverwrite: true,
-  packageManager: "npm",
-  // selectedTemplate: "react-js",
-  templateBranch: "create-package",
+  projectName: program.args[0],
+  selectedTemplate: options.template,
+  packageManager: options.packageManager,
+  templateBranch: options.branch,
 }).catch((err) => {
   console.error(err.message);
   process.exit(1);
