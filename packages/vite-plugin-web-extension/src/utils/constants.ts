25230b35e03d5501c922813d484a8e55ee8da722
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-01-29 13:17:13 -0600

    cleanup

--- packages/vite-plugin-web-extension/src/utils/constants.ts
+++ packages/vite-plugin-web-extension/src/utils/constants.ts
@@ -3,5 +3,4 @@ export const LABELED_STEP_PLUGIN_NAME = `web-extension:labeled-step`;
 export const MULTIBUILD_COMPLETE_PLUGIN_NAME = `web-extension:multibuild`;
 export const BUNDLE_TRACKER_PLUGIN_NAME = `web-extension:bundle-tracker`;
 export const DISABLE_DEV_PLUGIN_NAME = `web-extension:disable-dev`;
-export const HMR_PLUGIN_NAME = `web-extension:hmr`;
 export const HMR_REWRITE_PLUGIN_NAME = `web-extension:hmr-rewrite`;
