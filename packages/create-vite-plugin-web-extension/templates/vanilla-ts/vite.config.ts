0399feafdf291e77d063a5e3f0603d8ac9b3031f
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-03-03 16:49:43 -0600

    Upgrade vite configs to v2

--- packages/create-vite-plugin-web-extension/templates/vanilla-ts/vite.config.ts
+++ packages/create-vite-plugin-web-extension/templates/vanilla-ts/vite.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from "vite";
 import webExtension, { readJsonFile } from "vite-plugin-web-extension";
 
-function loadWebExtConfig() {
-  try {
-    return require("./.web-ext.config.json");
-  } catch {
-    return undefined;
-  }
-}
-
 function generateManifest() {
   const manifest = readJsonFile("src/manifest.json");
   const pkg = readJsonFile("package.json");
@@ -23,8 +15,6 @@ function generateManifest() {
 export default defineConfig({
   plugins: [
     webExtension({
-      assets: "public",
-      webExtConfig: loadWebExtConfig(),
       manifest: generateManifest,
     }),
   ],
--- packages/create-vite-plugin-web-extension/templates/vue-js/vite.config.js
+++ packages/create-vite-plugin-web-extension/templates/vue-js/vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import webExtension, { readJsonFile } from "vite-plugin-web-extension";
 
-function loadWebExtConfig() {
-  try {
-    return require("./.web-ext.config.json");
-  } catch {
-    return undefined;
-  }
-}
-
 function generateManifest() {
   const manifest = readJsonFile("src/manifest.json");
   const pkg = readJsonFile("package.json");
@@ -26,8 +18,6 @@ export default defineConfig({
   plugins: [
     vue(),
     webExtension({
-      assets: "public",
-      webExtConfig: loadWebExtConfig(),
       manifest: generateManifest,
     }),
   ],
