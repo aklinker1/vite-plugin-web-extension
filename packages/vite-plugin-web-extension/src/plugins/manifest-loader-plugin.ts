b959a8b6a5600ebd79ca898edb703f78ad805897
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-03-09 12:09:34 -0600

    Add comment

--- packages/vite-plugin-web-extension/src/plugins/manifest-loader-plugin.ts
+++ packages/vite-plugin-web-extension/src/plugins/manifest-loader-plugin.ts
@@ -112,6 +112,8 @@ export function manifestLoaderPlugin(options: ResolvedOptions): vite.Plugin {
       return vite.mergeConfig(
         {
           server: {
+            // Set the server origin so assets contain the entire url, not just the absolute path
+            // See #79
             origin: "http://127.0.0.1:5173",
           },
           build: {
