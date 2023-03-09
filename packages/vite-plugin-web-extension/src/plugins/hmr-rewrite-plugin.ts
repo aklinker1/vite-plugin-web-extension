f06958838a6caa9921a06936bb16afa16043985a
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-03-09 12:03:15 -0600

    fix: Use correct origin for assets in dev mode

--- packages/vite-plugin-web-extension/src/plugins/hmr-rewrite-plugin.ts
+++ packages/vite-plugin-web-extension/src/plugins/hmr-rewrite-plugin.ts
@@ -50,6 +50,7 @@ export function hmrRewritePlugin(config: {
 
       const hmrConfig: InlineConfig = {
         server: {
+          origin: "http://127.0.0.1:5173",
           hmr: {
             protocol: "http:",
             host: "127.0.0.1",
