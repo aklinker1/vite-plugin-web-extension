c74d1f0713ed0973f9e21d78e8d37d9852223e36
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-03-12 10:14:15 -0500

    Add react demo

---
+++ packages/demo-react/src/pages/Popup.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './Popup.css';
+
+function Popup() {
+  console.log("Hello from the popup!");
+
+  return (
+    <div>
+      <img src="/icon-with-shadow.svg" />
+      <h1>vite-plugin-web-extension</h1>
+      <p>
+        Template: <code>react-ts 2</code>
+      </p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.body).render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>
+);
---
+++ packages/demo-react/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    name: pkg.name,
+    description: pkg.description,
+    version: pkg.version,
+    ...manifest,
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    webExtension({
+      manifest: generateManifest,
+    }),
+  ],
+});
