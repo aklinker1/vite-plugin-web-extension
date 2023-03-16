b7c2e2f1eb49b5a1eb172e134e5bcb83faa31c8e
Amaral Krichman <amaralkrichman@gmail.com>
2023-03-16 12:36:53 -0300

    Add svelte-ts files

---
+++ packages/create-vite-plugin-web-extension/templates/svelte-ts/src/pages/Popup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  console.log("Hellow from the popup!");
+  let template = ${{ template.templateName }};
+</script>
+
+<div>
+  <img src="/icon-with-shadow.svg" />
+  <h1>vite-plugin-web-extension</h1>
+  <p>
+    Template: <code>{template}</code>
+  </p>
+</div>
+
+<style>
+
+</style>
