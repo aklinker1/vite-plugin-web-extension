5e542fc7af5e22337c8038f18b3cfccc2d7d120f
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-03-15 19:33:36 -0500

    Remove react demo

--- packages/demo-react/src/popup.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Popup from './pages/Popup';
-
-ReactDOM.createRoot(document.getElementById("app")!).render(
-  <React.StrictMode>
-    <Popup />
-  </React.StrictMode>
-);
