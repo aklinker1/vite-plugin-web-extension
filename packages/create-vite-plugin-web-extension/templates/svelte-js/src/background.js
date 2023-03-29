6058999416aa8b58e108760228b971b638da8700
Amaral Krichman <amaralkrichman@gmail.com>
2023-03-29 19:34:54 -0300

    chore: Add svelte template to starter kit (#93)

---
+++ packages/create-vite-plugin-web-extension/templates/svelte-js/src/background.js
@@ -0,0 +1,7 @@
+import browser from "webextension-polyfill";
+
+console.log("Hello from the background!");
+
+browser.runtime.onInstalled.addListener((details) => {
+  console.log("Extension installed:", details);
+});
---
+++ packages/create-vite-plugin-web-extension/templates/svelte-js/src/pages/Popup.svelte
@@ -0,0 +1,15 @@
+<script>
+  console.log("Hello from the popup!");
+</script>
+
+<div>
+  <img src="/icon-with-shadow.svg" alt="" />
+  <h1>vite-plugin-web-extension</h1>
+  <p>
+    Template: <code>${{ template.templateName }}</code>
+  </p>
+</div>
+
+<style>
+
+</style>
