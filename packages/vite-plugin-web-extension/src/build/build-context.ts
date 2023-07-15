7c41accb66845a5df82beb3c69131b9cd6f5c4ff
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-07-15 08:29:02 -0500

    fix order

--- packages/vite-plugin-web-extension/src/build/build-context.ts
+++ packages/vite-plugin-web-extension/src/build/build-context.ts
@@ -77,23 +77,26 @@ export function createBuildContext({
     });
     const totalEntries = entryConfigs.count;
 
-    // Merge the entrypoint config required to build something with some required config for child builds
-    return entryConfigs.all
-      .map((config, i) =>
-        vite.mergeConfig(config, {
-          // Reapply the mode passed into the top level build
-          mode: viteMode,
-          // We shouldn't clear the screen for these internal builds
-          clearScreen: false,
-          // Don't empty the outDir, this is handled in the parent build process
-          build: { emptyOutDir: false },
-          plugins: [
-            labeledStepPlugin(logger, totalEntries, i, paths),
-            multibuildManager.plugin(),
-          ],
-        })
-      )
-      .map((config) => vite.mergeConfig(config, userConfig));
+    return (
+      entryConfigs.all
+        // Merge user's vite.config.ts options first
+        .map((config) => vite.mergeConfig(config, userConfig))
+        // Overwrite some specific options, ignoring the user's config
+        .map((config, i) =>
+          vite.mergeConfig(config, {
+            // Reapply the mode passed into the top level build
+            mode: viteMode,
+            // We shouldn't clear the screen for these internal builds
+            clearScreen: false,
+            // Don't empty the outDir, this is handled in the parent build process
+            build: { emptyOutDir: false },
+            plugins: [
+              labeledStepPlugin(logger, totalEntries, i, paths),
+              multibuildManager.plugin(),
+            ],
+          })
+        )
+    );
   }
 
   function printSummary(
