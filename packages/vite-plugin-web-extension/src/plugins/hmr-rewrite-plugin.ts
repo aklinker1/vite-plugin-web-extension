2de1cf7f4fe30f9a2dc0a345b76d9e3f825fbdb0
Vasilii <vasiliicuhar@fastmail.com>
2024-05-31 17:03:44 +0300

    Update serializeDefine implementation from upstream vite

--- packages/vite-plugin-web-extension/src/plugins/hmr-rewrite-plugin.ts
+++ packages/vite-plugin-web-extension/src/plugins/hmr-rewrite-plugin.ts
@@ -24,15 +24,24 @@ export function hmrRewritePlugin(config: {
       : undefined;
 
   // Coped from node_modules/vite, do a global search for: vite:client-inject
-  function serializeDefine(define: any): string {
+  function serializeDefine(define: Record<string, any>): string {
     let res = `{`;
-    for (const key in define) {
+    const keys = Object.keys(define);
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
       const val = define[key];
-      res += `${JSON.stringify(key)}: ${
-        typeof val === "string" ? `(${val})` : JSON.stringify(val)
-      }, `;
+      res += `${JSON.stringify(key)}: ${handleDefineValue(val)}`;
+      if (i !== keys.length - 1) {
+        res += `, `;
+      }
     }
     return res + `}`;
+  }
+
+  function handleDefineValue(value: any): string {
+    if (typeof value === "undefined") return "undefined";
+    if (typeof value === "string") return value;
+    return JSON.stringify(value);
   }
 
   return {
